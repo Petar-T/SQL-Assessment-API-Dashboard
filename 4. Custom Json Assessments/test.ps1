3b01efc1ddfbaa90e3e07d366a820dfd7f2076f6
Petar Trbos <47648550+Petar-T@users.noreply.github.com>
2020-03-30 01:34:24 +0200

    Add files via upload

---
+++ 4. Custom Json Assessments/test.ps1
@@ -0,0 +1,11 @@
+﻿$samplesPath=Get-Location
+$serverInstance = Get-SqlInstance -ServerInstance 'localhost'
+
+
+#Invoke-SqlAssessment $serverInstance -configuration $(join-path $samplesPath "Assesment001Custom.json")
+#Invoke-SqlAssessment $serverInstance -Configuration $(join-path $samplesPath "Assesment001Custom.json"), $(join-path $samplesPath "Disable_All.json")
+Invoke-SqlAssessment $serverInstance -Configuration $(join-path $samplesPath "Assesment001Custom.json"), $(join-path $samplesPath "Disable_Default_Ruleset.json")
+
+
+
+
